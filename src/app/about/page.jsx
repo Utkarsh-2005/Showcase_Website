de057ba02805eb1c5fd3ad17c206f751d6e1d8a6
Utkarsh <utkarshjha.4009@gmail.com>
2023-08-28 12:50:35 +0530

    Initial commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+        <h1>AboutPage</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores tempore consectetur distinctio reprehenderit beatae inventore iure? Alias, non voluptatum! Velit, aspernatur? Quae asperiores vel at provident omnis, magnam nulla. Ullam?</p>
+    </div>
+  )
+}
+
+export default AboutPage;
---
+++ src/app/about/team/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TeamPage = () => {
+  return (
+    <div>
+        <h1>Our Team</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa cum rem distinctio recusandae repellat explicabo expedita enim, dicta possimus facilis tempora illo nihil tempore neque dolore autem obcaecati, quis facere!</p>
+    </div>
+  )
+}
+
+export default TeamPage;
